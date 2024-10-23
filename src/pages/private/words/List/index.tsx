89d00fc9e5f3665023d732d6418e8bb7e181737c
daviddiazh <daviddiazlds1221@gmail.com>
2024-10-22 21:14:15 -0500

    feat: get words rendered

--- src/pages/private/words/List/index.tsx
+++ src/pages/private/words/List/index.tsx
@@ -1,5 +1,38 @@
+import { useEffect, useState } from "react"
+import { useAuth } from "../../../../context/auth/AuthProvider";
+import { useAxios } from "../../../../hooks/useAxios";
+
 export const ListWords = () => {
-    return (
-      <div>ListWords</div>
-    )
+
+  const [ words, setWords ] = useState([]);
+
+  const { logout } = useAuth();
+  const { get } = useAxios({
+    logout
+  });
+
+  const fetch = async () => {
+    const resp = await get('/api/words')
+    setWords(resp)
+  }
+
+  useEffect(() => {
+    void fetch()
+  }, [])
+
+  return (
+    <div>
+      <h2 style={{ margin: '15px 0' }}>Listar las palabras</h2>
+      {
+        words?.length && words?.map((word: any) => (
+          <div key={word?.englishWord}>
+            <p>{word?.englishWord}</p>
+            <ol>
+              {word?.translations?.map(t => <li key={t+word?.englishWord}>{t}</li>)}
+            </ol>
+          </div>
+        ))
+      }
+    </div>
+  )
 }
